5e42984d05b545e353527082a082be69006e7861
Ansyar <m.ansyarafi@gmail.com>
2024-12-15 10:51:39 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+# update tag
 VERSION=$(git rev-parse --short HEAD)
 
 TAG=${VERSION} docker compose build
 TAG=${VERSION} docker compose push
+
 
 for file in ./tindog/*.yaml; do
   TAG=${VERSION} envsubst < "$file" > "$file"
@@ -13,4 +14,5 @@ for file in ./tindog/templates/*.yaml; do
   TAG=${VERSION} envsubst < "$file" > "$file"
 done
 
+# helm install
 helm install tindog . --namespace my-project
