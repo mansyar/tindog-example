4fafd07a98dcc4fc95ce5d8d14bd1babc2a9b3b1
Ansyar <m.ansyarafi@gmail.com>
2024-12-15 13:33:59 +0700

    edit deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 # update tag
 VERSION=$(git rev-parse --short HEAD)
 
-TAG=${VERSION} docker compose build
-TAG=${VERSION} docker compose push
+TAG=$VERSION docker compose build
+TAG=$VERSION docker compose push
 
 
 
-TAG=${VERSION} sed -i "s/\${TAG}/$TAG/g" ./tindog/values.yaml
+TAG=$VERSION sed -i "s/\${TAG}/$TAG/g" ./tindog/values.yaml
 
 # helm install
 sudo helm install tindog ./tindog --namespace my-project --kubeconfig /home/ansyar/kubeconfig.yaml
