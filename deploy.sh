784cc4c7d05b75db50914a9c0c4aba7215526288
Ansyar <m.ansyarafi@gmail.com>
2024-12-15 12:13:26 +0700

    edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,8 @@ for file in ./tindog/templates/*.yaml; do
   TAG=${VERSION} envsubst < "$file" > "$file"
 done
 
+cat ./tindog/Charts.yaml
+cat ./tindog/values.yaml
+
 # helm install
 sudo helm install --set name=tindog tindog ./tindog --namespace my-project
