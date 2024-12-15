1ddae0cb905c55123aaad43e87f2a4d8738f3da5
Ansyar <m.ansyarafi@gmail.com>
2024-12-15 12:52:19 +0700

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,8 @@ TAG=${VERSION} docker compose build
 TAG=${VERSION} docker compose push
 
 
-for file in ./tindog/*.yaml; do
-  TAG=${VERSION} envsubst < "$file" > "$file"
-done
 
-for file in ./tindog/templates/*.yaml; do
-  TAG=${VERSION} envsubst < "$file" > "$file"
-done
-
-ls
+TAG=${VERSION} envsubst < ./tindog/values.yaml > ./tindog/values.yaml
 
 # helm install
 sudo helm install --name tindog ./tindog --namespace my-project
